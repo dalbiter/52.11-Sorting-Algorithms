3a896031aeb7345bf738db05f1b751e83b73ab96
danx <rushn24@gmail.com>
2025-02-26 20:57:56 -0500

    bubbleSort3 iteration

--- app.js
+++ app.js
@@ -30,3 +30,22 @@ function bubbleSort2(arr) {
     console.log("TOTAL COUNT:", count)
     return arr;     
 }
+
+function bubbleSort3(arr) {
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        let swapped = false;
+        for(let j = 0; j < arr.length-i; j++){
+            count++
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                swapped = true;
+            }
+        }
+        if(!swapped) break;   
+    }
+    console.log("TOTAL COUNT:", count)
+    return arr;     
+}
